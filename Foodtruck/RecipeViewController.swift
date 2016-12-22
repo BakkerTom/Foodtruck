d8fd21baac04f1e717ea75b290b40d220134c988
Pas,Thom T.A.H.M. van de <thom.vandepas@student.fontys.nl>
2016-12-22 12:56:28 +0100

    Added Foodtrucks viewcontroller and made the first setup of the foodtruck implementation

--- Foodtruck/RecipeViewController.swift
+++ Foodtruck/RecipeViewController.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 class RecipeViewController: UIViewController {
-
+    
     @IBOutlet weak var webView: UIWebView!
     var recipe: Recipe?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if recipe != nil {
             
             let requestUrl = URL(string: (recipe?.url)!)
@@ -23,21 +23,21 @@ class RecipeViewController: UIViewController {
             webView.loadRequest(request)
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
